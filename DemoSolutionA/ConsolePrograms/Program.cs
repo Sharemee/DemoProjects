6777c5eee42213cc58dc5c230e2f93f1887a6b81
Sunshine <HK_EternalNabula@Outlook.com>
2019-07-09 16:18:13 +0800

    Demo1 Success

--- DemoSolutionA/ConsolePrograms/Program.cs
+++ DemoSolutionA/ConsolePrograms/Program.cs
@@ -10,13 +10,22 @@ namespace ConsolePrograms
     {
         static void Main(string[] args)
         {
-            int num1 = 2;
-            int num2 = 3;
-            num1 = num1 ^ num2;
-            Console.WriteLine(num1);
-
+            Console.WriteLine("当前系统时间: " + DateTime.Now.ToString() + "\n");
+            Demo1();
 
             Console.ReadKey();
         }
+
+        static void Demo1()
+        {
+            int num1 = 2;
+            int num2 = 3;
+            Console.WriteLine("原始结果:");
+            Console.WriteLine("num1=" + num1 + "\n" + "num2=" + num2);
+            Cryptology.异或运算_不使用第三个变量交换两值(num1, num2, out num1, out num2);
+            Console.WriteLine("运算后结果:");
+            Console.WriteLine("num1=" + num1 + "\n" + "num2=" + num2);
+        }
+
     }
 }
---
+++ DemoSolutionA/ConsolePrograms/密码学类.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePrograms
+{
+    /// <summary>
+    /// 密码学类
+    /// </summary>
+    public class Cryptology
+    {
+        public static void 异或运算_不使用第三个变量交换两值(int n1,int n2,out int r1,out int r2)
+        {
+            n1 = n1 ^ n2;
+            n2 = n1 ^ n2;
+            n1 = n1 ^ n2;
+            r1 = n1;
+            r2 = n2;
+        }
+    }
+}
