bc63ed320ab48ad3a734249656ef704fff413df8
Sunshine <HK_EternalNabula@Outlook.com>
2019-07-09 11:27:08 +0800

    Test som idea

---
+++ DemoSolutionA/ConsolePrograms/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 2;
+            int num2 = 3;
+            num1 = num1 ^ num2;
+            Console.WriteLine(num1);
+
+
+            Console.ReadKey();
+        }
+    }
+}
