eba4b0423139997ff562ea6f8cf940f447a3889c
Sunshine <HK_EternalNabula@Outlook.com>
2019-07-24 10:09:05 +0800

    Success

--- DemoSolutionA/ConsolePrograms/Program.cs
+++ DemoSolutionA/ConsolePrograms/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsolePrograms
@@ -11,11 +12,14 @@ namespace ConsolePrograms
         static void Main(string[] args)
         {
             Console.WriteLine("当前系统时间: " + DateTime.Now.ToString() + "\n");
-            Demo2();
+            Demo3();
 
             Console.ReadKey();
         }
 
+        /// <summary>
+        /// 异或运算_不使用第三个变量交换两值
+        /// </summary>
         static void Demo1()
         {
             int num1 = 2;
@@ -27,12 +31,23 @@ namespace ConsolePrograms
             Console.WriteLine("num1=" + num1 + "\n" + "num2=" + num2);
         }
 
+        /// <summary>
+        /// 获取本月月初时间
+        /// </summary>
         static void Demo2()
         {
             DateTime dt = DateTime.Today;
             Console.WriteLine("当前时间: "+dt.ToString());
             Console.WriteLine("--: "+(1-dt.Day));
             Console.WriteLine("本月月初: "+dt.AddDays(1-dt.Day));
+        }
+
+        static void Demo3()
+        {
+            DateTime dt = DateTime.Now;
+            Console.WriteLine("记录的时间: "+dt);
+            Thread.Sleep(3000);
+            Console.WriteLine("3秒钟之后取变量记录的时间: "+dt);
         }
     }
 }
