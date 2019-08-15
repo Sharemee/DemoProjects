b79190ddc7ce4c65e4d987e102c125205952abd0
Sunshine <HK_EternalNabula@Outlook.com>
2019-08-15 15:32:27 +0800

    Success

---
+++ DemoSolutionB/CheckCardId/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CheckCardId
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cardId = "360121198607318353";
+            Console.WriteLine(CheckCardIds(cardId));
+
+            Console.ReadKey();
+        }
+
+        static bool CheckCardIds(string id)
+        {
+            if (id.Length == 18)
+            {
+                return A(id);
+            }
+            else
+            {
+                if (id.Length == 15)
+                {
+                    return B(id);
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        static bool A(string str)
+        {
+            string reg18 = @"(^[0-9]{17}[x|S]$)|(^[0-9]{18})";
+            return Regex.IsMatch(str, reg18);
+        }
+
+        static bool B(string str)
+        {
+            string reg15 = @"^[0-9]{15}";
+            return Regex.IsMatch(str, reg15);
+        }
+    }
+}
