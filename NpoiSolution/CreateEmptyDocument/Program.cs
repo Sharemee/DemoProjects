e7c3b2490a8609f95c29ba59ad403ab4be9aa8be
Sunshine <HK_EternalNabula@Outlook.com>
2019-09-12 15:06:00 +0800

    Success

---
+++ NpoiSolution/CreateEmptyDocument/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using NPOI.XWPF.UserModel;
+
+namespace CreateEmptyDocument
+{
+    class Program
+    {
+        static void Main()
+        {
+            XWPFDocument doc = new XWPFDocument();
+            doc.CreateParagraph();
+            FileStream fs = File.Create("Blank.docx");
+            doc.Write(fs);
+            fs.Close();
+
+            Console.ReadKey();
+        }
+    }
+}
