dacc61e7c9b758c75b33d3ee5852fd7bacf70cb2
Sunshine <HK_EternalNabula@Outlook.com>
2019-09-27 17:40:58 +0800

    Update Demo.py

--- Note/Python/Demo.py
+++ Note/Python/Demo.py
@@ -1,8 +1,16 @@
-# -*- coding: utf-8 -*-
+# -*- coding:utf-8 -*-
 import requests
 
-response=requests.get("https://weibo.com")
+url_baidu='https://www.baidu.com/'
+url_baidu_s='https://www.baidu.com/s?'
+url_weibo='https://weibo.com/'
+wd={'wd':'python'}
+resp1=requests.get(url_baidu)
+resp2=requests.get(url_baidu_s,params=wd)
+#response=requests.get("https://weibo.com")
 
-print(response.status_code)
-print(response.cookies)
-print(response.text)
+
+print(resp1.status_code)
+print(resp1.cookies)
+print(resp1.text)
+print(resp2.text)
