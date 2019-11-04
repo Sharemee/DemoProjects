e1faa29237e607c7d32670953411438c92e59631
Sunshine <HK_EternalNabula@Outlook.com>
2019-11-04 20:24:49 +0800

    Success

---
+++ Html/Demo/dataTime/dataTime.js
@@ -0,0 +1,20 @@
+$(function(){
+	$('#datetimepicker2').datetimepicker({		
+		lang:'ch',
+		timepicker:false,
+		format:'Y-m-d',
+		formatDate:'Y-m-d',
+	});
+	$('#datetimepicker3').datetimepicker({		
+		lang:'ch',
+		timepicker:false,
+		format:'Y-m-d',
+		formatDate:'Y-m-d',
+	});
+	$('#date_birthdate').datetimepicker({
+	    lang: 'ch',
+	    timepicker: false,
+	    format: 'Y-m-d',
+	    formatDate: 'Y-m-d',
+	});
+});
